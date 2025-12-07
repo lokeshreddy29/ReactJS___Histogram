a2a93745bab45c6697e1d868882a23475cab03e7
lokeshreddy29 <lokeshr3ddy@gmail.com>
2025-12-07 12:28:43 +0530

    added function to extract and store count

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,57 @@ import "./App.css";
 import Histogram from "./Histogram";
 
 function App() {
-  const [characterCount, setCharacterCount] = useState(Array(26).fill(0));
+  const [characterCount, setCharacterCount] = useState([]);
   const [submit, setSubmit] = useState(false);
 
+  var enteredWord;
+
   const handleClick = (word) => {
+    if (!word) return;
+
     setSubmit(!submit);
     extractCharacters(word);
   };
 
   const extractCharacters = (word) => {
-    
-  }
+    const splitString = word.split("");
+    // ['a', 'b', 'c']
+    // this is retuning thr ASCII value of the character at 0th index in the split string.
+    // when i remove 97 from the ASCII value that is giving the corresponding index for that character
+    // i can then use the index to store the count of that particular character which can then be used by the Histogram component.
+    const newArray = Array(26).fill(0);
+
+    for (let i = 0; i < splitString.length; i++) {
+      var charIndex = splitString[i].charCodeAt(0) - 97;
+      var charCount = 0;
+
+      for (let j = 0; j < splitString.length; j++) {
+        if (splitString[i] === splitString[j]) charCount++;
+      }
+      newArray[charIndex] = charCount;
+    }
+
+    setCharacterCount(newArray);
+  };
 
   return (
     <>
       <h1 className="m-3">Alphabet Histogram</h1>
-
+      {console.log(characterCount)}
       {!submit ? (
         <div>
-          <input className="rounded-md m-2 p-2 h-11 bg-zinc-900" />
-          <button onClick={() => handleClick("word")}>Generate</button>
+          <input
+            className="rounded-md m-2 p-2 h-11 bg-zinc-900"
+            onChange={(e) => (enteredWord = e.target.value.toLowerCase())}
+          />
+          <button onClick={() => handleClick(enteredWord)}>Generate</button>
         </div>
       ) : (
         <div>
           <div className="h-100 w-300 border border-white grid grid-flow-col place-items-end grid-cols-26">
             <Histogram count={10} />
           </div>
-          <button onClick={() => handleClick("word")}>Edit text</button>
+          <button onClick={() => setSubmit(!submit)}>Edit text</button>
         </div>
       )}
     </>
